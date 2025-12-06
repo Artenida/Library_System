c9d20e4bf938d49cb09a31df6f852c65eeb8a637
Artenida <artenidadaci@gmail.com>
2025-12-06 11:30:49 +0100

    Ensured consistent route mounting in app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import authRoutes from "./routes/authRoutes"
+import userRoutes from "./routes/userRoutes"
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ app.use(cookieParser());
 const PORT = 5000; 
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript + Node.js server is running!');
