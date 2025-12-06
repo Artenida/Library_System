0e8029f7441bd1b695c0f0b83af7a5d364826527
Artenida <artenidadaci@gmail.com>
2025-12-06 16:36:55 +0100

    Use author routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import cookieParser from 'cookie-parser';
 import express from 'express';
 import authRoutes from "./routes/authRoutes"
 import userRoutes from "./routes/userRoutes"
+import authorRoutes from "./routes/authorRoutes"
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,7 @@ const PORT = 5000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/author", authorRoutes)
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript + Node.js server is running!');
