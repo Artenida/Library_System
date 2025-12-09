35f04f24e344842d8f7b223890c917b6d84f179d
Artenida <artenidadaci@gmail.com>
2025-12-10 00:05:53 +0100

    Fix problems with tailwind configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,17 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import AppRoutes from "./routes/AppRoutes";
 import { Provider } from "react-redux";
 import { store, persistor } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
+   <React.StrictMode>
+    <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <AppRoutes />
+      <App />
     </PersistGate>
-  </Provider>
+    </Provider>  
+  </React.StrictMode>
 );
