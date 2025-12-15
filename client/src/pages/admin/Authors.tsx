88e60ac09eba6f4baa53d662f68dc52a1537ec6d
Artenida <artenidadaci@gmail.com>
2025-12-15 22:01:41 +0100

    Author update logic implemented

--- client/src/pages/admin/Authors.tsx
+++ client/src/pages/admin/Authors.tsx
@@ -1,9 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { CircularProgress, Container, Box } from "@mui/material";
-import { getAuthorsThunk } from "../../store/thunks/authorThunks";
+import {
+  deleteAuthorThunk,
+  getAuthorsThunk,
+  updateAuthorThunk,
+} from "../../store/thunks/authorThunks";
 import AuthorsTable from "../../components/admin/AuthorsTable";
 import { useNavigate } from "react-router-dom";
+import EditAuthorModal from "../../components/modals/EditAuthorModal";
 
 interface IAuthor {
   author_id: string;
@@ -16,12 +21,38 @@ const Authors = () => {
   const navigate = useNavigate();
   const { authors, loading } = useAppSelector((state) => state.author);
 
+  const [editingAuthor, setEditingAuthor] = useState<IAuthor | null>(null);
+
   useEffect(() => {
     dispatch(getAuthorsThunk());
   }, [dispatch]);
 
   const handleRowClick = (author: IAuthor) => {
     navigate(`/dashboard/authors/${author.author_id}`);
+  };
+
+  const handleDelete = (author: IAuthor) => {
+    if (window.confirm(`Delete author "${author.name}"?`)) {
+      dispatch(deleteAuthorThunk(author.author_id));
+    }
+  };
+
+  const handleEdit = (author: IAuthor) => {
+    setEditingAuthor(author);
+  };
+
+  const handleSaveEdit = (updatedAuthor: IAuthor) => {
+    dispatch(
+      updateAuthorThunk({
+        id: updatedAuthor.author_id,
+        data: {
+          name: updatedAuthor.name,
+          birth_year: updatedAuthor.birth_year,
+        },
+      })
+    );
+
+    setEditingAuthor(null);
   };
 
   if (loading)
@@ -33,7 +64,18 @@ const Authors = () => {
 
   return (
     <Container sx={{ mt: 3 }}>
-      <AuthorsTable authors={authors} onRowClick={handleRowClick} />
+      <AuthorsTable
+        authors={authors}
+        onRowClick={handleRowClick}
+        onEdit={handleEdit}
+        onDelete={handleDelete}
+      />
+      <EditAuthorModal
+        open={Boolean(editingAuthor)}
+        author={editingAuthor}
+        onClose={() => setEditingAuthor(null)}
+        onSave={handleSaveEdit}
+      />
     </Container>
   );
 };
