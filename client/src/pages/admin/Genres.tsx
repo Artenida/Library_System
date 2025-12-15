e4236ec3b075c49e7177965d41eff9072c1a9a60
Artenida <artenidadaci@gmail.com>
2025-12-15 22:17:45 +0100

    Update and delete genre implemented

--- client/src/pages/admin/Genres.tsx
+++ client/src/pages/admin/Genres.tsx
@@ -1,9 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { CircularProgress, Container, Box } from "@mui/material";
-import { getGenresThunk } from "../../store/thunks/genreThunks";
+import {
+  deleteGenreThunk,
+  getGenresThunk,
+  updateGenreThunk,
+} from "../../store/thunks/genreThunks";
 import GenresTable from "../../components/admin/GenresTable";
 import { useNavigate } from "react-router-dom";
+import EditGenreModal from "../../components/modals/EditGenreModal";
 
 interface IGenre {
   genre_id: string;
@@ -15,12 +20,36 @@ const Genres = () => {
   const navigate = useNavigate();
   const { genres, loading } = useAppSelector((state) => state.genre);
 
+  const [editingGenre, setEditingGenre] = useState<IGenre | null>(null);
+
   useEffect(() => {
     dispatch(getGenresThunk());
   }, [dispatch]);
 
   const handleRowClick = (genre: IGenre) => {
     navigate(`/dashboard/genres/${genre.genre_id}`);
+  };
+  const handleEdit = (genre: IGenre) => {
+    setEditingGenre(genre);
+  };
+
+  const handleDelete = (genre: IGenre) => {
+    if (window.confirm(`Delete genre "${genre.name}"?`)) {
+      dispatch(deleteGenreThunk(genre.genre_id));
+    }
+  };
+
+  const handleSaveEdit = (updatedGenre: IGenre) => {
+    dispatch(
+      updateGenreThunk({
+        id: updatedGenre.genre_id,
+        data: {
+          name: updatedGenre.name,
+        },
+      })
+    );
+
+    setEditingGenre(null);
   };
 
   if (loading)
@@ -32,7 +61,18 @@ const Genres = () => {
 
   return (
     <Container sx={{ mt: 3 }}>
-      <GenresTable genres={genres} onRowClick={handleRowClick} />
+      <GenresTable
+        genres={genres}
+        onRowClick={handleRowClick}
+        onEdit={handleEdit}
+        onDelete={handleDelete}
+      />
+      <EditGenreModal
+        open={Boolean(editingGenre)}
+        genre={editingGenre}
+        onClose={() => setEditingGenre(null)}
+        onSave={handleSaveEdit}
+      />
     </Container>
   );
 };
